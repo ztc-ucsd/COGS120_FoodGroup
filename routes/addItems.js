b2fa2d790b2f2f1513cffff4c53e17333b4b502f
ztc-ucsd <60235467+ztc-ucsd@users.noreply.github.com>
2020-02-20 12:11:55 -0800

    s

--- routes/addItems.js
+++ routes/addItems.js
@@ -2,7 +2,7 @@ var data = require("../data2.json");
 
 exports.addItems = function(request, response) {
    
-	// Your code goes here
+   // Your code goes here
    var item = {
       "itemName": request.query.itemName,
       "expiration": "2 weeks",
--- routes/items.js
+++ routes/items.js
@@ -1,3 +1,4 @@
+
 var data = require('../data2.json');
 
 exports.view = function (request, response) {
@@ -12,9 +13,10 @@ exports.view = function (request, response) {
    var ind = section.lastIndexOf('/');
    section = section.substring(ind); //Gets the actual section from the url
    console.log("Section: " + section);*/
+
    console.log(data);
    data['Sections'][0]['categories'][1]['items'].push(test);
    response.render('items.handlebars', data);
 
    response.render('itemsScreen.js', data);
-};
+}
