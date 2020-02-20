3eb38c85e0cc26dca34d52b0d57f4a4701446def
ztc-ucsd <ztc@ucsd.edu>
2020-02-20 12:53:27 -0800

    small

--- routes/items.js
+++ routes/items.js
@@ -2,17 +2,19 @@
 var data = require('../data2.json');
 
 exports.view = function (request, response) {
-
-	 var test = {
-	 	"itemName": "YESSSS",
-		"expiration": "2020-02-19",
-		"notification": "3 day",
-		"shared": "TRUE"
-
+   var test = {
+      "itemName": "YESSSS",
+      "expiration": "2020-02-19",
+      "notification": "3 day",
+      "shared": "TRUE"
    }
 
+   /*var section = window.location.href; //Current section passed in url from '/sections' page
+   var ind = section.lastIndexOf('/');
+   section = section.substring(ind); //Gets the actual section from the url
+   console.log("Section: " + section);*/
 
    console.log(data);
    data['Sections'][0]['categories'][1]['items'].push(test);
    response.render('items.handlebars', data);
-};
+}
