7ba7b93a983af81ee0c13b301b47efa9e406ce04
ztc-ucsd <60235467+ztc-ucsd@users.noreply.github.com>
2020-02-20 11:14:39 -0800

    not working

--- routes/items.js
+++ routes/items.js
@@ -1,8 +1,20 @@
 
 var data = require('../data.json');
+var item =
+   {
+      "itemName": "TEST",
+      "section": "Fridge",
+      "category": "Dairy",
+      "expiration": "2 weeks",
+      "notification": "3 day",
+      "shared": "TRUE"
+   }
+data.items.push(item);
 
 exports.view = function (request, response) {
 
    console.log(data);
    response.render('items.handlebars', data);
+
+   response.render('itemsScreen.js', data);
 };
